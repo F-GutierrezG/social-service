bcd98546b0587c4850b33e980e24e88d33a098cb
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-23 11:30:50 -0300

    facebook routes enable

--- src/project/__init__.py
+++ src/project/__init__.py
@@ -11,8 +11,10 @@ migrate = Migrate()
 
 def register_blueprints(app):
     from project.views.health import health_blueprint
+    from project.views.facebook import facebook_blueprint
 
     app.register_blueprint(health_blueprint)
+    app.register_blueprint(facebook_blueprint)
     pass
 
 
